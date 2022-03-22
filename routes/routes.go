2fc4c25d1a96fbb5074364d5ece227f25949631f
zooeymoon <kimmi-moon@163.com>
2022-03-22 21:59:04 +0800

    add gitignore

---
+++ routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func Routes() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	return r
+}
